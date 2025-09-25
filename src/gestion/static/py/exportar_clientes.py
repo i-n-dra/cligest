26115156e633e2f49f9e85c16af88e039f557fa3
indra <ayasem33@gmail.com>
2025-09-25 09:36:20 -0300

    exportar_clientes.py

--- src/gestion/static/py/exportar_clientes.py
+++ src/gestion/static/py/exportar_clientes.py
@@ -41,18 +41,135 @@ ficha_border = Border(
 )
 
 def exportar_clientes_main(clientes=QuerySet):
-    path = 'D:/py_venvs/proyectos/proyecto_integracion/cligest/Clientes.xlsx'
-    wb = load_workbook(path)
-    if not wb:
-        create_file()
+    path = 'D:/py_venvs/proyectos/proyecto_integracion/cligest/FichasClientes.xlsx'
+    try:
+        wb = load_workbook(path)
+    except FileNotFoundError:
+        msg = 'No se encontró el archivo "FichasClientes.xlsx" en el directorio, se ha creado uno nuevo'
+        wb = Workbook(write_only=False)
+        create_file(wb)
 
     # grab the active worksheet
     ws = wb.active
 
-    # wip
-    print(clientes)
-    for c in clientes:
-        pass
+    # agregar clientes
+    # falta probar con mas de uno xd
+    n_row = 3
+    while n_row<(clientes.__len__())+3:
+        for c in clientes.all():
+            print(f'ingresando cliente: {c.id}')
+            ws.cell(
+                n_row,
+                column = 1,
+                value = c.nombre_rep_legal
+            )
+            ws.cell(
+                n_row,
+                2,
+                c.last_name_1_rep_legal
+            )
+            ws.cell(
+                n_row,
+                3,
+                c.last_name_2_rep_legal
+            )
+            ws.cell(
+                n_row,
+                4,
+                c.razon_social
+            )
+            ws.cell(
+                n_row,
+                5,
+                c.nombre_fantasia
+            )
+            ws.cell(
+                n_row,
+                6,
+                c.run_rep_legal
+            )
+            ws.cell(
+                n_row,
+                7,
+                c.run_empresa
+            )
+            ws.cell(
+                n_row,
+                8,
+                c.tipo_empresa
+            )
+            ws.cell(
+                n_row,
+                9,
+                str(c.reg_tributario)
+            )
+            ws.cell(
+                n_row,
+                10,
+                c.giro_rubro
+            )
+            ws.cell(
+                n_row,
+                11,
+                c.codigo_sii.code
+            )
+            ws.cell(
+                n_row,
+                12,
+                str(c.tipo_contabilidad)
+            )
+            ws.cell(
+                n_row,
+                13,
+                c.cuenta_corriente
+            )
+            ws.cell(
+                n_row,
+                14,
+                c.n_cuenta_corriente
+            )
+            ws.cell(
+                n_row,
+                15,
+                c.email
+            )
+            ws.cell(
+                n_row,
+                16,
+                c.phone_number
+            )
+            ws.cell(
+                n_row,
+                17,
+                str(c.region)
+            )
+            ws.cell(
+                n_row,
+                18,
+                str(c.comuna)
+            )
+            ws.cell(
+                n_row,
+                19,
+                c.address
+            )
+
+            n_row += 1
+
+    for column in ws.columns:
+        max_length = 0
+        col = column[1].column_letter # Get the column letter (e.g., 'A', 'B')
+        for cell in column:
+            if cell.value is not None:
+                curr_length = len(str(cell.value))
+                if curr_length > max_length and max_length < 80:
+                    max_length = curr_length
+
+        # Add padding to the width
+        adjusted_width = (max_length + 2) * 1.2 
+        ws.column_dimensions[col].width = adjusted_width
+
+    ws.row_dimensions[1].height = 24
 
     # Save the file
 
@@ -64,14 +181,15 @@ def exportar_clientes_main(clientes=QuerySet):
 
     # opción 2
     try:
-        wb.save('Clientes.xlsx') 
+        wb.save('FichasClientes.xlsx') 
         # + guardar una copia en el escritorio?
+        if not msg:
+            msg = 'Se ha creado "FichasClientes.xlsx" exitosamente'
+            return msg
     except PermissionError:
         raise Http404('Por favor, cierre el archivo antes de confirmar los cambios')
 
-def create_file():
-    wb = Workbook(write_only=False)
-
+def create_file(wb):
     # grab the active worksheet
     ws = wb.active
     ws.title = "Clientes"
@@ -114,18 +232,3 @@ def create_file():
     for row in subtitle_cells:
         for c in row:
             c.font = subtitle_font
-
-    for column in ws.columns:
-        max_length = 0
-        col = column[1].column_letter # Get the column letter (e.g., 'A', 'B')
-        for cell in column:
-            if cell.value is not None:
-                curr_length = len(str(cell.value))
-                if curr_length > max_length:
-                    max_length = curr_length
-
-        # Add padding to the width
-        adjusted_width = (max_length + 2) * 1.2 
-        ws.column_dimensions[col].width = adjusted_width
-
-    ws.row_dimensions[1].height = 24
